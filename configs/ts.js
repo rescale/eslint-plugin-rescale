5683aa30f5ef6cc6df8b8bdd983cb9c095d4d1d5
Steven Snyder <steven@rescale.com>
2019-05-01 14:01:50 -0700

    add "TODO"s to to-dos

--- configs/ts.js
+++ configs/ts.js
@@ -3,12 +3,13 @@ module.exports = {
   rules: {
     // already covered by base
     '@typescript-eslint/indent': 'ignore',
-    // should be enabled when we normalize on `import`
+    // TODO: should be enabled when we normalize on `import`
     '@typescript-eslint/no-var-requires': 'ignore',
-    // should be enabled (IMO) but many components rely on `function` hoisting
+    // TODO: should be enabled (IMO) but many components rely on `function` hoisting
     '@typescript-eslint/no-use-before-define': 'ignore',
-    // could be enabled easily, many components destructure bits they don't actually need out of props or state
+    // TODO:could be enabled easily, many components destructure bits they don't actually need out of props or state
     '@typescript-eslint/no-unused-vars': 'ignore',
+
     '@typescript-eslint/explicit-member-accessibility': [ 'no-public', 'error' ],
     '@typescript-eslint/camelcase': 'ignore',
   }
